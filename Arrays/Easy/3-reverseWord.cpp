b1ca2d8a82bae4a2d7223a9dccf64fe848c9d8bc
Raj Bhanani <92816813+RajBhanani@users.noreply.github.com>
2024-04-12 21:09:42 +0530

    Create 3-reverseWord.cpp

---
+++ Arrays/Easy/3-reverseWord.cpp
@@ -0,0 +1,11 @@
+string reverseWord(string str)
+    {
+        int start = 0, end = str.size() - 1;
+        while(start < end)
+        {
+            swap(str[start], str[end]);
+            start++;
+            end--;
+        }
+        return str;
+    }
