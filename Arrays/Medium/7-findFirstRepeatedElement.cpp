39b3070402ddbc82c036442052eb5d0f562d3e7c
Raj Bhanani <92816813+RajBhanani@users.noreply.github.com>
2024-04-12 21:53:02 +0530

    Create 7-findFirstRepeatedElement.cpp

---
+++ Arrays/Medium/7-findFirstRepeatedElement.cpp
@@ -0,0 +1,13 @@
+int firstRepeated(int arr[], int n)
+    {
+        int lowest = n + 1;
+        unordered_map<int, int> mp;
+        for (int i = n - 1; i >= 0; --i)
+        {
+          if (mp.find(arr[i]) != mp.end())
+            lowest = i + 1;
+          else
+            mp[arr[i]]++;
+        }
+        return lowest == n + 1 ? -1 : lowest;
+    }
